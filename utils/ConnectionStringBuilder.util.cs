32545548122f355dc34e9d7fa7fd5fbbc8779bf1
Carlos Turbides <81496355+cturbides@users.noreply.github.com>
2025-04-14 14:56:26 +0000

    Added new utils and Ci/Cd directory with Dockerfile for migrator job and db initialization script

---
+++ utils/ConnectionStringBuilder.util.cs
@@ -0,0 +1,16 @@
+namespace CRUDCxC.Utils
+{
+    public static class ConnectionStringBuilder
+    {
+        public static string BuildFromEnvironment()
+        {
+            var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+            var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            var dbUser = Environment.GetEnvironmentVariable("DB_USER");
+            var dbServer = Environment.GetEnvironmentVariable("DB_SERVER");
+            var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            return $"Server={dbServer},{dbPort};Database={dbName};User Id={dbUser};Password={dbPassword};Encrypt=False;";
+        }
+    }
+}
